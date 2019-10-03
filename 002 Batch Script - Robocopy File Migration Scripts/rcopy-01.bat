83c0a74d501a6f91763b2225bb1ff675d23e3b3d
wandersick <wandersick@gmail.com>
2019-10-03 12:48:10 +0800

    Change /E to /MIR in robocopy script

--- 002 Batch Script - Robocopy File Migration Scripts/rcopy-01.bat
+++ 002 Batch Script - Robocopy File Migration Scripts/rcopy-01.bat
@@ -1,3 +1,4 @@
-robocopy \\file-server\d$\home\staff D:\home\staff *.* /E /COPYALL /R:0 /W:0 /TEE /LOG+:robocopy_output.log /ZB /MT:32
+REM Be careful of /MIR. As a safe practice and habit, use /E instead where possible except final delta sync before switchover to prevent human mistakes which could cause data loss
+robocopy \\file-server\d$\home\staff D:\home\staff *.* /MIR /COPYALL /R:0 /W:0 /TEE /LOG+:robocopy_output.log /ZB /MT:32
 
 pause
